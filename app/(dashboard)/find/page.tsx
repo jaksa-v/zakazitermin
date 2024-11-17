f2902592de4d42f25ade7e1f4ac7ba359f02a110
Jaksa Vlahovic <jaksa.vlahovic1@gmail.com>
2024-11-17 14:03:32 +0100

    initial layout

---
+++ app/(dashboard)/find/page.tsx
@@ -0,0 +1,11 @@
+export default function FindPage() {
+  return (
+    <main>
+      <section className="py-20">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <h1>Find Page</h1>
+        </div>
+      </section>
+    </main>
+  );
+}
