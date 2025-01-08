1e8391c3136700e24e22a73bf11c0501807b2b26
Jaksa Vlahovic <jaksa.vlahovic1@gmail.com>
2025-01-08 19:11:37 +0100

    make the admin dashboard mobile friendly

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -17,12 +17,14 @@ export default async function DashboardPage() {
 
   return (
     <CourtProvider>
-      <div className="w-full max-w-screen-lg mx-auto px-4 sm:px-6 lg:px-8 sm:py-4">
-        <div className="flex items-center justify-between mb-8">
-          <h1 className="my-3 sm:my-4 text-xl sm:text-2xl font-bold">
+      <div className="w-full max-w-screen-lg mx-auto px-4 sm:px-6 lg:px-8 py-4 sm:py-6">
+        <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4 sm:gap-8 mb-6 sm:mb-8">
+          <h1 className="text-xl sm:text-2xl font-bold">
             Dashboard
           </h1>
-          <VenueCourtSelector venues={venues} />
+          <div className="w-full sm:w-auto">
+            <VenueCourtSelector venues={venues} />
+          </div>
         </div>
         <Suspense
           fallback={
